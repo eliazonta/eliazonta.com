5bb69d6ab2af62f95b924805d692a43c740afa12
Elia Zonta <elia.zontaa@gmail.com>
2026-05-28 18:23:07 +0200

    website refresh

--- components/icons.tsx
+++
@@ -1,7 +0,0 @@
-// components/icons.ts
-import GithubIcon from '../public/icons/github.svg'
-import TwitterIcon from '../public/icons/twitter.svg'
-import InstagramIcon from '../public/icons/instagram.svg'
-import LinkedinIcon from '../public/icons/linkedin.svg'
-
-export { GithubIcon, TwitterIcon, InstagramIcon, LinkedinIcon }
--- types/tex.d.ts
+++
@@ -1,4 +0,0 @@
-declare module '*.tex' {
-  const content: string
-  export default content
-} 
